15608bb84b2e532217f836943d53f02c4d1f11c6
Limyes <975096654@qq.com>
2018-07-19 10:25:31 +0800

    [fix]

--- resume/js/message.js
+++ resume/js/message.js
@@ -1,69 +1,66 @@
 !function(){
-  var view = document.querySelector('section.message')
-  var model = {
-    // 获取数据
-    init: function(){
+  var view=document.querySelector('section.message');
+  var controller={
+    view:null,
+    messageList:null,
+    form:null,
+    init:function(view){
+      this.view=view;
+      this.messageList=this.view.querySelector('#messageList');
+      this.form=this.view.querySelector('#postMessageForm');
+      this.initAV();
+      this.loadMessages();
+      this.bindEvents();
+    },
+    initAV:function(){
       var APP_ID = 'Hq30oxE0ccnYaUEu43ogcbRM-gzGzoHsz';
       var APP_KEY = '8biXDYkWtvbAtkCTPOuWDIG3';
-      AV.init({ appId: APP_ID, appKey: APP_KEY })
+      AV.init({
+        appId: APP_ID,
+        appKey: APP_KEY
+      });
     },
-    fetch: function(){ 
-      var query = new AV.Query('Message');
-      return query.find() // Promise 对象
+    loadMessages:function(){
+      var query=new AV.Query('Message');
+      query.find()
+      .then(
+        function(messages){
+          let array=messages.map((item)=>item.attributes);
+          array.forEach((item)=>{
+            let li=document.createElement('li');
+            li.innerText=`${item.name}:${item.content}`;
+            this.messageList.appendChild(li);
+          })
+        },
+        function(error){
+          alert("提交失败，请重新提交！")
+        }
+      ).then(()=>{},(error)=>{console.log(error);})
     },
-    // 创建数据
-    save: function(name, content){
-      var Message = AV.Object.extend('Message');
-      var message = new Message();
-      return message.save({  // Promise 对象
-        'name': name,
-        'content': content
+    bindEvents:function(){
+       this.form.addEventListener('submit',function(e){
+        e.preventDefault();
+        saveMessage.call();
       })
+    },
+    saveMessage:function(){
+        let myForm=this.form;
+        let name=myForm.querySelector('input[name=name]').value;
+        let content=myForm.querySelector('input[name=content]').value;
+        let Message= AV.Object.extend('Message');
+        let message = new Message();
+        message.save({
+          name:name,
+          content: content
+        }).then(function(object) {
+          let li=document.createElement('li');
+          li.innerText=`${object.attributes.name}:${object.attributes.content}`;
+          var messageList=document.querySelector('#messageList');
+          messageList.appendChild(li);
+          myForm.querySelector('input[name=name]').value='';
+          myForm.querySelector('input[name=content]').value='';
+        })
     }
   }
-  var controller = {
-    view: null,
-    model: null,
-    messageList: null,
-    init: function(view, model){
-      this.view = view
-      this.model = model
-      this.messageList = view.querySelector('#messageList')
-      this.form = view.querySelector('form')
-      this.model.init()
-      this.loadMessages()
-      this.bindEvents()
-    },
-    loadMessages: function(){
-      this.model.fetch().then(
-        (messages) => {
-          let array = messages.map((item)=> item.attributes )
-          array.forEach((item)=>{
-            let li = document.createElement('li')
-            li.innerText = `${item.name}: ${item.content}`
-            this.messageList.appendChild(li)
-          })
-        } 
-      )
-    },
-    saveMessage: function(){
-      let myForm = this.form
-      let content = myForm.querySelector('input[name=content]').value
-      let name = myForm.querySelector('input[name=name]').value
-      this.model.save(name, content).then(function(object) {
-        let li = document.createElement('li')
-        li.innerText = `${object.attributes.name}: ${object.attributes.content}`
-        let messageList = document.querySelector('#messageList')
-        messageList.appendChild(li)
-        myForm.querySelector('input[name=content]').value = '';
-      })
-    },
-    bindEvents: function(){
-      this.form.addEventListener('submit', function(e){
-        e.preventDefault()
-        this.saveMessage()
-      })
-    }, 
-  }
-  controller.init(view, model)
+controller.init(view);
 }.call()
